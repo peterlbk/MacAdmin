ae9213b854d48d5674577bd11c7c2ef284e44e43
Peter Loobuyck <peter.loobuyck@dpgmedia.be>
2025-04-25 12:15:11 +0200

    intial

---
+++ ApplicationsUsage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if app is installed
+APP="/Applications/$4"
+if [ -z "$APP" ]; then
+    echo "<result>N/A</result>"
+else
+
+# Get use count
+USECOUNT=$(mdls -name kMDItemUseCount "$APP" | awk '{ print $3 }')
+
+    if [[ "$USECOUNT" == *"null"* ]]; then
+        echo "<result>UNUSED</result>"
+    else
+        echo "<result>${USECOUNT}</result>"
+    fi
+fi
